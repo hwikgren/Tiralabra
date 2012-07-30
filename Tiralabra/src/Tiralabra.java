c4940d9be0cdc6c8e31f04c05362296ce69005c6
Heidi Jauhiainen <wikgren@gmail.com>
2012-07-30 18:55:23 +0300

    Luotiin projekti

---
+++ Tiralabra/src/Tiralabra.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author heidi
+ */
+public class Tiralabra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
